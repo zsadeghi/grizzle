87e621428c74c8c00cb07897deb33eb63af21bfb
Zohreh Sadehgi <zsadeghi@uw.edu>
2018-02-27 19:15:13 -0800

    Fix build issues

---
+++ grizzle-explorer/src/main/java/me/theyinspire/projects/grizzle/explorer/Launcher.java
@@ -0,0 +1,13 @@
+package me.theyinspire.projects.grizzle.explorer;
+
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(useDefaultFilters = false)
+@EnableAutoConfiguration
+public class Launcher {
+
+}
--- grizzle-import/src/main/java/me/theyinspire/projects/grizzle/importer/Launcher.java
+++ grizzle-import/src/main/java/me/theyinspire/projects/grizzle/importer/Launcher.java
@@ -10,9 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@ComponentScan(
-        useDefaultFilters = false
-)
+@ComponentScan(useDefaultFilters = false)
 @EnableAutoConfiguration
 @Import({
                 InputConfiguration.class,
